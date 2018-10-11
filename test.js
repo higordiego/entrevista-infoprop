5bf256068627ca88917297c3e9b84bc41bb9bcfe
Higor Diego <higordiegoti@gmail.com>
2018-10-10 21:19:54 -0300

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const csvFilePath = './import.csv'
-const csv = require('csvtojson')
-csv()
-    .fromFile(csvFilePath)
-    .then((jsonObj) => console.log(jsonObj[0]))
-    .catch(err => console.log(err))
